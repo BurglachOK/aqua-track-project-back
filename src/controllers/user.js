5424346079c6252b4dde7b8e542c3007da921fbc
mepinit <evgenmep@gmail.com>
2024-12-04 22:24:49 +0200

    updateUserController

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,55 @@
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 import { getCurrentUser, updateUser, getTotalUsers } from '../services/user.js';
+import { filterResUser } from '../utils/filterResUser.js';
+import createHttpError from 'http-errors';
+import { env } from '../utils/env.js';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
-export const updateUserController = async (req, res) => {
-  if (!req.user || !req.user._id) {
-    return res.status(401).json({ message: 'Unauthorized: user not found' });
+// export const updateUserController = async (req, res) => {
+//   if (!req.user || !req.user._id) {
+//     return res.status(401).json({ message: 'Unauthorized: user not found' });
+//   }
+
+//   const { ...userData } = req.body;
+//   const avatar = req.file ? await saveFileToCloudinary(req.file) : null;
+
+//   const updatedUser = await updateUser(req.user._id, { ...userData, avatar });
+
+//   res.json({
+//     status: 200,
+//     message: 'User was updated',
+//     data: updatedUser,
+//   });
+// };
+
+export const updateUserController = async (req, res, next) => {
+  const { _id: userId } = req.user;
+  const data = req.body;
+  let photoUrl;
+
+  if (req.file) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(req.file, 'users');
+    } else {
+      photoUrl = await saveFileToUploadDir(req.file, 'users');
+    }
   }
 
-  const { ...userData } = req.body;
-  const avatar = req.file ? await saveFileToCloudinary(req.file) : null;
+  const updatedResult = await updateUser(userId, {
+    ...data,
+    avatar: photoUrl,
+  });
 
-  const updatedUser = await updateUser(req.user._id, { ...userData, avatar });
+  if (!updatedResult) {
+    return next(createHttpError(404, 'User not found'));
+  }
+
+  const userWithoutTimestamps = filterResUser(updatedResult.user);
 
   res.json({
     status: 200,
-    message: 'User was updated',
-    data: updatedUser,
+    message: 'User information successfully updated!',
+    data: { updatedResult: userWithoutTimestamps },
   });
 };
 
@@ -52,7 +87,6 @@ export const getTotalUsersController = async (req, res) => {
   }
 };
 
-
 //Розкоментити коли треба буде отримувати на тотал юзери фото і ім'я
 // export const getTotalUsersController = async (req, res) => {
 //   try {
@@ -64,6 +98,5 @@ export const getTotalUsersController = async (req, res) => {
 //     });
 //   }
 // };
-
 
 //Ще у сервісах коменти з приводу тотал узерів
